750565be84dd12c8fed1514141a602d36fb30811
Tilak Oli <tilak.81oli@gmail.com>
2022-07-31 21:43:10 +0545

    coming soon

--- pages/index.js
+++ pages/index.js
@@ -6,12 +6,12 @@ export default function Home() {
       <Head>
         <title>Bideo background test</title>
       </Head>
-      <div className="h-screen w-screen z-50 relative">
-        <div className="fixed h-full w-full">
+      <div className="relative z-50 w-screen h-screen">
+        <div className="fixed w-full h-full">
           <BackgroundVideo className="absolute overlay -z-10" />
         </div>
-        <div className="text-black text-3xl relative z-50 h-screen w-screen  opacity-10  flex items-center justify-center">
-          here askbaksb
+        <div className="relative z-50 flex items-center justify-center w-screen h-screen text-3xl text-yellow-500 underline ">
+          Coming Soon!
         </div>
       </div>
     </>
